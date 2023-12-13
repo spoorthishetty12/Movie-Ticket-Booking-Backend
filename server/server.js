f6e09e30e74f3b458c9249565275b2597f25a97c
PTShivaji <ptshivaji8@gmail.com>
2023-12-13 17:22:49 +0530

    moviesRouites done

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ const dbConfig = require("./config/dbConfig")
 app.use(express.json())
 
 const usersRoute = require("./routes/usersRoute");
+const moviesRoute = require("./routes/moviesRoute");
 
 app.use("/api/users", usersRoute);
+app.use("/api/movies", moviesRoute);
 
 const port = process.env.PORT || 5000;
 
